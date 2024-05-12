d7fc2dbddde7d852656ff5eb969cad6178891fdd
MarioMuco <mucomario4@gmail.com>
2024-05-12 15:12:42 +0200

    new app

--- server/routes/User.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signUp, login } = require("../controllers/UserController");
-
-const router = express.Router();
-
-router.post("/login", login);
-router.post("/signup", signUp);
-
-module.exports = router;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import * as HotelsActions from './features/hotel/store/hotels.actions';
-import { Store } from '@ngrx/store';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { LoadingService } from "./services/loading.service";
+import { Subscription } from "rxjs";
+import { AuthService } from "./services/auth-services/auth.service";
+import { NavigationEnd, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  template: ` <router-outlet></router-outlet> `,
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
-  constructor(private store: Store) {}
+export class AppComponent implements OnInit, OnDestroy {
+  isLoading = false;
+  auth = false;
+  webmaster = false;
+  loadingSub: Subscription;
+  userSub: Subscription;
+  loadedAuthPage: boolean;
+  routerSub: Subscription;
+
+  constructor(
+    private l: LoadingService,
+    private authS: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.store.dispatch(HotelsActions.loadHotels());
+    this.authS.autoLogin();
+    this.loadingSub = this.l.isLoading.subscribe((isLoading) => {
+      this.isLoading = isLoading;
+    });
+
+    this.userSub = this.authS.User.subscribe((user) => {
+      this.auth = !!user;
+      if (user) this.webmaster = user.webmaster;
+      else this.webmaster = false;
+    });
+
+    this.routerSub = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd)
+        this.loadedAuthPage = this.router.url === "/auth" ? true : false;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.loadingSub.unsubscribe();
+    this.userSub.unsubscribe();
+    this.routerSub.unsubscribe();
+  }
+
+  onLogin() {
+    this.router.navigate(["auth"]);
+  }
+
+  onLogout() {
+    this.authS.logout();
   }
 }
